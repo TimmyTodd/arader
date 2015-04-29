bec0e671b1207ee3c11276c68bc41362d697d894
Todd Carpenter <toddbc@gmail.com>
2015-04-29 10:45:19 -0400

    added bu to ignore

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
 		<meta name="generator" content="Bootply" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -113,12 +114,12 @@
   <!-- Three columns of text below the carousel -->
   <div class="row">
     <div class="col-md-4 text-center">
-      <img class="img-circle" src="http://placehold.it/140x140">
+      <!-- <img class="img-circle" src="http://placehold.it/140x140">
       <h2>Mobile-first</h2>
       <p>Tablets, phones, laptops. The new 3 promises to be mobile friendly from the start.</p>
-      <p><a class="btn btn-default" href="#">View details »</a></p>
+      <p><a class="btn btn-default" href="#">View details »</a></p> -->
     </div>
-    <div class="col-md-4 text-center">
+    <!-- <div class="col-md-4 text-center">
       <img class="img-circle" src="http://placehold.it/140x140">
       <h2>One Fluid Grid</h2>
       <p>There is now just one percentage-based grid for Bootstrap 3. Customize for fixed widths.</p>
@@ -129,7 +130,7 @@
       <h2>LESS is More</h2>
       <p>Improved support for mixins make the new Bootstrap 3 easier to customize.</p>
       <p><a class="btn btn-default" href="#">View details »</a></p>
-    </div>
+    </div> -->
   </div><!-- /.row -->
 
 
@@ -166,8 +167,13 @@
 
   <!-- FOOTER -->
   <footer>
-    <p class="pull-right"><a href="#">Back to top</a></p>
+    <!-- <p class="pull-right"><a href="#">Back to top</a></p> -->
     <p>Arader Tree Service</p>
+    <ul id="social" class="pull-right">
+      <li><a href=""><i class="fa fa-facebook-square"></i></a></li>
+      <li><a href=""><i class="fa fa-linkedin-square"></i></a></li>
+      <li><a href=""><i class="fa fa-google-plus-square"></i></a></li>
+    </ul>
   </footer>
 
 </div><!-- /.container -->
