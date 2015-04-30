c02d270f2c0c7f2b1b17c10d412df0fab94274cd
Todd Carpenter <toddbc@gmail.com>
2015-04-30 13:52:38 -0400

    style updates

--- public/inc/footer.php
+++ public/inc/footer.php
@@ -4,11 +4,12 @@
       <!-- FOOTER -->
       <footer>
         <ul id="social" class="pull-right">
-          <li><a href=""><i class="fa fa-facebook-square fa-3x"></i></a></li>
-          <li><a href=""><i class="fa fa-linkedin-square fa-3x"></i></a></li>
-          <li><a href=""><i class="fa fa-google-plus-square fa-3x"></i></a></li>
+          <li><a href="https://www.facebook.com/pages/Arader-Tree-Service/436949546415195?fref=ts" title="Arader on Facebook" target="_blank"><i class="fa fa-facebook-square fa-3x"></i></a></li>
+          <li><a href="https://www.linkedin.com/company/arader-tree-services-inc-" title="Arader on LinkedIn" target="_blank"><i class="fa fa-linkedin-square fa-3x"></i></a></li>
+          <li><a href="https://plus.google.com/+AradertreePA-TreeService/posts" title="Arader on Google+" target="_blank"><i class="fa fa-google-plus-square fa-3x"></i></a></li>
         </ul>
-        <p>&copy; 2015 Arader Tree Service</p>
+        <p>&copy; <?php echo date("Y"); ?> Arader Tree Service &nbsp;&middot;&nbsp; <a href="tel:+6102778733">(610) 277-8733</a><br />
+        <span class="small">10 Balligomingo Road suite 2A, West Conshohocken, PA 19428</span></p>
       </footer>
 
     </div><!-- /.container -->
@@ -24,5 +25,16 @@
     <script src="js/vendor/holder.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <script>
+      // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      // ga('create', 'UA-353923-4', 'auto');
+      // ga('send', 'pageview');
+    </script>
+
   </body>
 </html>
